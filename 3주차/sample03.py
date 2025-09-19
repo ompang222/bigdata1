e62812aca0e3140fc6e574462f8a321c3b8eccb2
214 <gugu00327@naver.com>
2025-09-19 09:51:02 +0900

    3주차 실습

---
+++ 3주차/sample03.py
@@ -0,0 +1,17 @@
+my_book = 2010, '자바', 200
+
+print(my_book)
+
+print('-'*30)
+
+print(my_book[0])
+
+year = my_book[0]
+
+print(year)
+
+year = 2025
+
+print(year)
+
+my_book[0] = 2025
---
+++ 3주차/sample12.py
@@ -0,0 +1,12 @@
+my_book = (2010, '자바', 300)
+your_book = [2010, '자바', 200]
+
+print(my_book)
+print(your_book)
+
+print('-'*30)
+your_book[0] = 2025
+your_book.append('자바책입니다.')
+your_book.remove('자바')
+
+print(your_book)
